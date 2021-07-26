8bbf9ad3139a9174af7b335b6b5aa78923e882c2
Hui Peng Hu <woohp135@gmail.com>
2021-07-26 01:10:52 -0700

    lots of updates

--- setup.sh
+++ setup.sh
@@ -3,8 +3,11 @@ mkdir $HOME/.config
 ln -sf "$PWD/nvim" $HOME/.config/nvim
 ln -sf "$PWD/zshrc" $HOME/.zshrc
 ln -sf "$PWD/gitignore" $HOME/.gitignore
+ln -sf "$PWD/rgignore" $HOME/.rgignore
 
 tic xterm-256color.ti
 
-defaults write -g ApplePressAndHoldEnabled -bool false
+if [ `uname` == "Darwin" ]; then
+    defaults write -g ApplePressAndHoldEnabled -bool false
+fi
 mkdir $HOME/.vim_swap
