7a73c0a9293182e855a4eec94c1e77bb98e45930
Hui Peng Hu <woohp135@gmail.com>
2025-09-18 10:01:39 -0700

    lazyvim update

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -41,3 +41,10 @@ if not vim.b.jinja_syntax_autocmd_loaded then
   end
   vim.b.jinja_syntax_autocmd_loaded = true
 end
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "python" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
